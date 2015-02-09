4e13f25a70785262a43f994eaa8442c4ea199949
okadahiroyuki <okdhryk@gmail.com>
2015-02-09 22:10:24 +0900

    OpenHRI

---
+++ trcp_OpenHRI/scripts/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#cd /home/ito/Dropbox/Programing/workspace/121104_consoleIn/src
+cd ~/catkin_ws/src/trcp/trcp_OpenHRI/scripts 
+
+#start component
+pulseaudioinput &
+pulseaudiooutput &
+openjtalkrtc &
+juliusrtc sample.xml &
+#python ConsoleOut.py &
+seat sample.seatml &
+sleep 10;
+
+#スクリプトからは下記コマンドが無効でした。
+#rtcwd 127.0.0.1/
+
+#active component
+rtact 127.0.0.1/ubuntu.host_cxt/PulseAudioInput0.rtc
+rtact 127.0.0.1/ubuntu.host_cxt/JuliusRTC0.rtc
+rtact 127.0.0.1/ubuntu.host_cxt/SEAT0.rtc
+#rtact 127.0.0.1/ubuntu.host_cxt/ConsoleOut0.rtc
+rtact 127.0.0.1/ubuntu.host_cxt/OpenJTalkRTC0.rtc
+rtact 127.0.0.1/ubuntu.host_cxt/PulseAudioOutput0.rtc
+
+#connect component
+rtcon 127.0.0.1/ubuntu.host_cxt/PulseAudioInput0.rtc:AudioDataOut 127.0.0.1/ubuntu.host_cxt/JuliusRTC0.rtc:data
+rtcon 127.0.0.1/ubuntu.host_cxt/JuliusRTC0.rtc:result 127.0.0.1/ubuntu.host_cxt/SEAT0.rtc:speechin
+rtcon 127.1.0.1/ubuntu.host_cxt/SEAT0.rtc:speechout 127.0.0.1/ubuntu.host_cxt/OpenJTalkRTC0.rtc:text
+#rtcon 127.0.0.1/ubuntu.host_cxt/SEAT0.rtc:commandout 127.0.0.1/ubuntu.host_cxt/ConsoleOut0.rtc:in
+rtcon 127.0.0.1/ubuntu.host_cxt/OpenJTalkRTC0.rtc:result 127.0.0.1/ubuntu.host_cxt/PulseAudioOutput0.rtc:AudioDataIn
+

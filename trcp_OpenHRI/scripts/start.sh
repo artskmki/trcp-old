429757b9b08fde5412133fe41a8111d82e55ba10
hiroyukiokada <okdhryk@gmail.com>
2015-02-14 09:51:51 +0900

    hello

--- trcp_OpenHRI/scripts/start.sh
+++ trcp_OpenHRI/scripts/start.sh
@@ -18,15 +18,15 @@ sleep 10;
 #rtcwd 127.0.0.1/
 
 #active component
-rtact 127.0.0.1/ubuntu.host_cxt/PulseAudioInput0.rtc
-rtact 127.0.0.1/ubuntu.host_cxt/JuliusRTC0.rtc
-rtact 127.0.0.1/ubuntu.host_cxt/eSEAT0.rtc
-rtact 127.0.0.1/ubuntu.host_cxt/OpenJTalkRTC0.rtc
-rtact 127.0.0.1/ubuntu.host_cxt/PulseAudioOutput0.rtc
+rtact 127.0.0.1/M14xR2.host_cxt/PulseAudioInput0.rtc
+rtact 127.0.0.1/M14xR2.host_cxt/JuliusRTC0.rtc
+rtact 127.0.0.1/M14xR2.host_cxt/eSEAT0.rtc
+rtact 127.0.0.1/M14xR2.host_cxt/OpenJTalkRTC0.rtc
+rtact 127.0.0.1/M14xR2.host_cxt/PulseAudioOutput0.rtc
 
 #connect component
-rtcon 127.0.0.1/ubuntu.host_cxt/PulseAudioInput0.rtc:AudioDataOut 127.0.0.1/ubuntu.host_cxt/JuliusRTC0.rtc:data
-rtcon 127.0.0.1/ubuntu.host_cxt/JuliusRTC0.rtc:result 127.0.0.1/ubuntu.host_cxt/eSEAT0.rtc:speechin
-rtcon 127.1.0.1/ubuntu.host_cxt/eSEAT0.rtc:speechout 127.0.0.1/ubuntu.host_cxt/OpenJTalkRTC0.rtc:text
-rtcon 127.0.0.1/ubuntu.host_cxt/OpenJTalkRTC0.rtc:result 127.0.0.1/ubuntu.host_cxt/PulseAudioOutput0.rtc:AudioDataIn
+rtcon 127.0.0.1/M14xR2.host_cxt/PulseAudioInput0.rtc:AudioDataOut 127.0.0.1/M14xR2.host_cxt/JuliusRTC0.rtc:data
+rtcon 127.0.0.1/M14xR2.host_cxt/JuliusRTC0.rtc:result 127.0.0.1/M14xR2.host_cxt/eSEAT0.rtc:speechin
+rtcon 127.1.0.1/M14xR2.host_cxt/eSEAT0.rtc:speechout 127.0.0.1/M14xR2.host_cxt/OpenJTalkRTC0.rtc:text
+rtcon 127.0.0.1/M14xR2.host_cxt/OpenJTalkRTC0.rtc:result 127.0.0.1/M14xR2.host_cxt/PulseAudioOutput0.rtc:AudioDataIn
 

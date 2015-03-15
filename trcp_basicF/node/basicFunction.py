172de7761560a7f50dc9759237ee7a1bf25bef8b
Hiroyuki Okada <okdhryk@gmail.com>
2015-03-15 22:40:39 +0900

    a

--- trcp_basicF/node/basicFunction.py
+++ trcp_basicF/node/basicFunction.py
@@ -3,50 +3,185 @@
 import rospy
 import smach
 import smach_ros
+from smach import State, StateMachine
+from smach_ros import SimpleActionState, IntrospectionServer
+from geometry_msgs.msg import Twist
+import easygui
+import datetime
+from collections import OrderedDict
+from trcp_basicF.task_setup import *
 
-# define state Foo
-class Foo(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1','outcome2'])
-        self.counter = 0
+# A list of rooms and tasks
+task_list = {'pp_room':['mop_floor'], 'at_room':['mop_floor'], 'wdys_room':['mop_floor'], 'leaving_arena':['mop_floor']}
+
+class MopFloor(State):
+    def __init__(self, room, timer):
+        State.__init__(self, outcomes=['succeeded','aborted','preempted'])
+
+        self.task = 'mop_floor'
+        self.room = room
+        self.timer = timer
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist)
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state FOO')
-        if self.counter < 3:
-            self.counter += 1
-            return 'outcome1'
+        rospy.loginfo('Mopping the floor in the ' + str(self.room))
+        cmd_vel_msg = Twist()
+        cmd_vel_msg.linear.x = 0.05
+        cmd_vel_msg.angular.z = 1.2
+        counter = self.timer
+        while counter > 0:
+            self.cmd_vel_pub.publish(cmd_vel_msg)
+            cmd_vel_msg.linear.x *= -1
+            rospy.loginfo(counter)
+            counter -= 1
+            rospy.sleep(1)
+
+        self.cmd_vel_pub.publish(Twist())
+        message = "Done mopping the " + str(self.room) + "!"
+        rospy.loginfo(message)
+        easygui.msgbox(message, title="Succeeded")
+
+        update_task_list(self.room, self.task)
+
+        return 'succeeded'
+
+
+
+def update_task_list(room, task):
+    task_list[room].remove(task)
+    if len(task_list[room]) == 0:
+        del task_list[room]
+
+class main():
+    def __init__(self):
+        rospy.init_node('clean_house', anonymous=False)
+
+        # Set the shutdown function (stop the robot)
+        rospy.on_shutdown(self.shutdown)
+
+        # Initialize a number of parameters and variables
+        setup_task_environment(self)
+
+        # Turn the room locations into SMACH move_base action states
+        nav_states = {}
+
+        for room in self.room_locations.iterkeys():
+            nav_goal = MoveBaseGoal()
+            nav_goal.target_pose.header.frame_id = 'map'
+            nav_goal.target_pose.pose = self.room_locations[room]
+            move_base_state = SimpleActionState('move_base', MoveBaseAction, goal=nav_goal, result_cb=self.move_base_result_cb,
+                                                exec_timeout=rospy.Duration(15.0),
+                                                server_wait_timeout=rospy.Duration(10.0))
+            nav_states[room] = move_base_state
+
+        # Create a state machine for the pp room subtask(s)
+        sm_pp_room = StateMachine(outcomes=['succeeded','aborted','preempted'])
+        # Then add the subtask(s)
+        with sm_pp_room:
+            StateMachine.add('MOP_FLOOR', MopFloor('pp_room', 5), transitions={'succeeded':'','aborted':'','preempted':''})
+
+
+
+        # Create a state machine for the at room subtask(s)
+        sm_at_room = StateMachine(outcomes=['succeeded','aborted','preempted'])
+        # Then add the subtask(s)
+        with sm_at_room:
+            StateMachine.add('MOP_FLOOR', MopFloor('at_room', 5), transitions={'succeeded':'','aborted':'','preempted':''})
+
+
+
+        # Create a state machine for the wdys room subtask(s)
+        sm_wdys_room = StateMachine(outcomes=['succeeded','aborted','preempted'])
+        # Then add the subtask(s)
+        with sm_wdys_room:
+            StateMachine.add('MOP_FLOOR', MopFloor('wdys_room', 5), transitions={'succeeded':'','aborted':'','preempted':''})
+
+
+
+        # Create a state machine for the wdys room subtask(s)
+        sm_leaving_arena = StateMachine(outcomes=['succeeded','aborted','preempted'])
+        # Then add the subtask(s)
+        with sm_leaving_arena:
+            StateMachine.add('MOP_FLOOR', MopFloor('leaving_arena', 5), transitions={'succeeded':'','aborted':'','preempted':''})
+
+
+        # Create a state machine for the hallway subtask(s)
+        sm_hallway = StateMachine(outcomes=['succeeded','aborted','preempted'])
+        # Then add the subtasks
+        with sm_hallway:
+            StateMachine.add('MOP_FLOOR', MopFloor('hallway', 5), transitions={'succeeded':'','aborted':'','preempted':''})
+
+
+        # Initialize the overall state machine
+        sm_basic_f = StateMachine(outcomes=['succeeded','aborted','preempted'])
+        # Build the basic f state machine from the nav states 
+        # and task states
+        with sm_basic_f:
+            StateMachine.add('START', nav_states['hallway'], transitions={'succeeded':'PP_ROOM','aborted':'PP_ROOM','preempted':'PP_ROOM'})
+
+            StateMachine.add('PP_ROOM', nav_states['pp_room'], transitions={'succeeded':'PP_ROOM_TASKS','aborted':'AT_ROOM','preempted':'AT_ROOM'})
+
+            # When the tasks are done, continue on to the AT_ROOM 
+            StateMachine.add('PP_ROOM_TASKS', sm_pp_room, transitions={'succeeded':'AT_ROOM','aborted':'AT_ROOM','preempted':'AT_ROOM'})
+
+            StateMachine.add('AT_ROOM', nav_states['at_room'], transitions={'succeeded':'AT_ROOM_TASKS','aborted':'WDYS_ROOM','preempted':'WDYS_ROOM'})
+
+            # When the tasks are done, continue on to the wdys_room
+            StateMachine.add('AT_ROOM_TASKS', sm_at_room, transitions={'succeeded':'WDYS_ROOM','aborted':'WDYS_ROOM','preempted':'WDYS_ROOM'})
+
+            StateMachine.add('WDYS_ROOM', nav_states['wdys_room'], transitions={'succeeded':'WDYS_ROOM_TASKS','aborted':'LEAVING_ARENA','preempted':'LEAVING_ARENA'})
+
+            # When the tasks are done, leaving to the arean
+            StateMachine.add('WDYS_ROOM_TASKS', sm_wdys_room, transitions={'succeeded':'LEAVING_ARENA','aborted':'LEAVING_ARENA','preempted':'LEAVING_ARENA'})
+
+            StateMachine.add('LEAVING_ARENA', nav_states['leaving_arena'], transitions={'succeeded':'LEAVING_ARENA_TASKS','aborted':'','preempted':''})
+
+            # When the tasks are done, stop
+            StateMachine.add('LEAVING_ARENA_TASKS', sm_leaving_arena, transitions={'succeeded':'','aborted':'','preempted':''})
+
+       # Create and start the SMACH introspection server
+        intro_server = IntrospectionServer('clean_house', sm_basic_f, '/SM_ROOT')
+        intro_server.start()
+
+        # Execute the state machine
+        sm_outcome = sm_basic_f.execute()
+
+        if len(task_list) > 0:
+            message = "Ooops! Not all chores were completed."
+            message += "The following rooms need to be revisited: "
+            message += str(task_list)
         else:
-            return 'outcome2'
+            message = "All chores complete!"
 
-# define state Bar
-class Bar(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['outcome1'])
+        rospy.loginfo(message)
+        easygui.msgbox(message, title="Finished Cleaning")
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state BAR')
-        return 'outcome1'
+        intro_server.stop()
 
 
-def main():
-    rospy.init_node('smach_example_state_machine')
 
-    # Create a SMACH state machine
-    sm = smach.StateMachine(outcomes=['outcome4'])
 
-    # Open the container
-    with sm:
-        # Add states to the container
-        smach.StateMachine.add('FOO', Foo(),
-                               transitions={'outcome1':'BAR', 'outcome2':'outcome4'})
-        smach.StateMachine.add('BAR', Bar(),
-                               transitions={'outcome1':'FOO'})
 
-    # Execute SMACH plan
-    outcome = sm.execute()
 
+
+
+
+
+    def move_base_result_cb(self, userdata, status, result):
+        if status == actionlib.GoalStatus.SUCCEEDED:
+            pass
+
+
+    def shutdown(self):
+        rospy.loginfo("Stopping the robot...")
+        #sm_nav.request_preempt()
+        self.cmd_vel_pub.publish(Twist())
+        rospy.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("House clearning test finished.")
 
--- trcp_basicF/src/trcp_basicF/task_setup.py
+++ trcp_basicF/src/trcp_basicF/task_setup.py
@@ -11,6 +11,103 @@ from math import  pi
 from collections import OrderedDict
 
 def setup_task_environment(self):
-    rospy.loginfo("Waiting for ...")
+    # How long do we have to get to each waypoint?
+    self.move_base_timeout = rospy.get_param("~move_base_timeout", 10) #seconds
+
+    # Subscribe to the move_base action server
+    self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+
+    rospy.loginfo("Waiting for move_base action server...")
+
+    # Wait up to 60 seconds for the action server to become available
+    self.move_base.wait_for_server(rospy.Duration(60))
+
+    rospy.loginfo("Connected to move_base action server")
+    
+    # create a list to hold the target quaternions (orientations)
+    quaternions = list()
+
+    # First define the corner orientations as Euler angles
+    euler_angles = (pi/2, pi, 3*pi/2, 0)
+
+    # Then convert the angles to quaternions
+    for angle in euler_angles:
+        q_angle = quaternion_from_euler(0, 0, angle, axes='sxyz')
+        q = Quaternion(*q_angle)
+        quaternions.append(q)
+
+    # Create a list to hold the waypoint poses
+    self.waypoints = list()
+
+    # Append each of the four waypoints to the list.  Each waypoint
+    # is a pose consisting of a position and orientation in the map frame.
+    self.waypoints.append(Pose(Point(0.0, 0.0, 0.0), quaternions[3]))
+    self.waypoints.append(Pose(Point(1.0, 0.0, 0.0), quaternions[0]))
+    self.waypoints.append(Pose(Point(1.0, 1.0, 0.0), quaternions[1]))
+    self.waypoints.append(Pose(Point(0.0, 1.0, 0.0), quaternions[2]))
+    self.waypoints.append(Pose(Point(2.0, 1.0, 0.0), quaternions[2]))
+
+
+    # Create a mapping of room names to waypoint locations
+    room_locations = (('hallway', self.waypoints[0]),
+                      ('pp_room', self.waypoints[1]),
+                      ('at_room', self.waypoints[2]),
+                      ('wdys_room', self.waypoints[3]),
+                      ('leaving_arena', self.waypoints[4]))
+
+    # Store the mapping as an ordered dictionary 
+    # so we can visit the rooms in sequence
+    self.room_locations = OrderedDict(room_locations)
+
+    # Initialize the waypoint visualization markers for RViz
+    init_waypoint_markers(self)
+
+    # Set a visualization marker at each waypoint
+    for waypoint in self.waypoints:
+        p = Point()
+        p = waypoint.position
+        self.waypoint_markers.points.append(p)
+
+
+    # Publisher to manually control the robot (e.g. to stop it)
+    self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist)
+
+    rospy.loginfo("Starting Tasks")
+
+    # Publish the waypoint markers
+    self.marker_pub.publish(self.waypoint_markers)
+    rospy.sleep(1)
+    self.marker_pub.publish(self.waypoint_markers)
     
     rospy.sleep(1)
+
+def init_waypoint_markers(self):
+    # Set up our waypoint markers
+    marker_scale = 0.2
+    marker_lifetime = 0 # 0 is forever
+    marker_ns = 'waypoints'
+    marker_id = 0
+    marker_color = {'r': 1.0, 'g': 0.7, 'b': 1.0, 'a': 1.0}
+
+    # Define a marker publisher.
+    self.marker_pub = rospy.Publisher('waypoint_markers', Marker)
+
+    # Initialize the marker points list.
+    self.waypoint_markers = Marker()
+    self.waypoint_markers.ns = marker_ns
+    self.waypoint_markers.id = marker_id
+    self.waypoint_markers.type = Marker.CUBE_LIST
+    self.waypoint_markers.action = Marker.ADD
+    self.waypoint_markers.lifetime = rospy.Duration(marker_lifetime)
+    self.waypoint_markers.scale.x = marker_scale
+    self.waypoint_markers.scale.y = marker_scale
+    self.waypoint_markers.color.r = marker_color['r']
+    self.waypoint_markers.color.g = marker_color['g']
+    self.waypoint_markers.color.b = marker_color['b']
+    self.waypoint_markers.color.a = marker_color['a']
+
+    self.waypoint_markers.header.frame_id = 'odom'
+    self.waypoint_markers.header.stamp = rospy.Time.now()
+    self.waypoint_markers.points = list()
+
+

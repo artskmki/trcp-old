e2ec0aa8c369e408fff884ee46a024cd6c59f13d
Hiroyuki Okada <okdhryk@gmail.com>
2015-03-19 20:02:31 +0900

    a

---
+++ trcp_openhri/scripts/startFolloMw.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+cd ~/catkin_ws/src/trcp/trcp_OpenHRI/scripts 
+
+
+#start component
+pulseaudioinput &
+pulseaudiooutput &
+openjtalkrtc &
+festivalrtc &
+juliusrtc followme.xml &
+#python ConsoleOut.py &
+python rosRTMfollowme.py &
+cd ../eSeat
+#seat sample.seatml &
+python eSEAT.py followme.seatml &
+
+sleep 5; 
+
+#スクリプトからは下記コマンドが無効でした。
+#rtcwd 127.0.0.1/
+
+host=/localhost/`hostname`.host_cxt
+
+#active component
+rtact $host/PulseAudioInput0.rtc
+rtact $host/PulseAudioOutput0.rtc
+rtact $host/JuliusRTC0.rtc
+rtact $host/OpenJTalkRTC0.rtc
+rtact $host/FestivalRTC0.rtc
+sleep 5;
+rtact $host/eSEAT0.rtc
+rtact $host/rosRTMfollowme0.rtc
+
+#connect component
+rtcon $host/PulseAudioInput0.rtc:AudioDataOut $host/JuliusRTC0.rtc:data
+rtcon $host/JuliusRTC0.rtc:result $host/eSEAT0.rtc:speechin
+rtcon $host/eSEAT0.rtc:speechout $host/OpenJTalkRTC0.rtc:text
+rtcon $host/eSEAT0.rtc:speechoutE $host/FestivalRTC0.rtc:text
+rtcon $host/OpenJTalkRTC0.rtc:result $host/PulseAudioOutput0.rtc:AudioDataIn
+rtcon $host/FestivalRTC0.rtc:result $host/PulseAudioOutput0.rtc:AudioDataIn
+rtcon $host/eSEAT0.rtc:command $host/rosRTMfollowme0.rtc:input_long
+

240e1765eecc4138ae8261be94346158f48ee2a2
Hiroyuki Okada <okdhryk@gmail.com>
2015-10-04 16:05:57 +0900

    only trcp

--- trcp_basic_function/src/listener.cpp
+++ trcp_basic_function/src/listener.cpp
@@ -4,6 +4,11 @@
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
+  
+  char ccc[128];
+  strcpy(ccc, msg->data.c_str());
+  printf("MSG:%s\n",ccc);
+
 }
 
 int main(int argc, char **argv)
@@ -11,8 +16,25 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-  ros::spin();
+ ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+ ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter2", 1000);
+  ros::spinOnce();
+
+ros::Rate loop_rate(10);
+while(ros::ok())
+{
+printf("AAAAAAAAAAAAAAAAAAAAA\n");
+    std_msgs::String msg;
+
+    std::string sss="return";
+    msg.data = sss;
+
+    chatter_pub.publish(msg);
+    ros::spinOnce();
+
+
+  loop_rate.sleep();
+}
 
   return 0;
 }
